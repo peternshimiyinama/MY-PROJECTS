9130af25551ac4d55f47a7ad24e1aabc64ebe1f2
peternshimiyinama <peternshimiyimana123@gmail.com>
2024-01-23 22:33:26 +0200

    Created sign up and login page

---
+++ Clothes store/CODES/index.js
@@ -0,0 +1,6 @@
+function createAcount(){
+    var account=document.getElementById("signUpAccount");
+    var remove=document.getElementById("loginSection");
+    account.style.visibility="visible";
+    remove.style.visibility="hidden";
+}
