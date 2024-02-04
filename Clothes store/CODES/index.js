49afaf21b795922bc0c6c8700b894ed137dda0b6
peternshimiyinama <peternshimiyimana123@gmail.com>
2024-02-04 10:39:06 +0200

    Created some web behaviors on login section

--- Clothes store/CODES/index.js
+++ Clothes store/CODES/index.js
@@ -1,10 +1,24 @@
-function createAcount(){
-    var account=document.getElementById("signUpAccount");
-    var remove=document.getElementById("loginSection");
-    account.style.visibility="visible";
-    remove.style.visibility="hidden";
+function login() {
+    var login_page = document.getElementById("loginSection");
+    var account = document.getElementById("signUpAccount");
+    if (account.style.visibility === "visible") {
+        account.style.visibility = "hidden";
+    }
+    if (login_page.style.visibility === "visible") {
+        login_page.style.visibility = "hidden";
+    } else {
+        login_page.style.visibility = "visible";
+    }
 }
-function login(){
-    var login_page=document.getElementById("loginSection");
-    login_page.style.visibility="visible";
+
+function createAcount() {
+    var account = document.getElementById("signUpAccount");
+    var login_section = document.getElementById("loginSection");
+    if (account.style.visibility === "visible") {
+        account.style.visibility = "hidden";
+    } else {
+        account.style.visibility = "visible";
+    }
+    login_section.style.visibility = "hidden";
 }
+
